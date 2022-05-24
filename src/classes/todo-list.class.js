39fcc84bb515cfef6b823e6ba32bf3ba5152d50d
Pamui <pablo.fceye@icloud.com>
2022-05-24 08:23:00 +0100

    22.01.00

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,25 @@
+
+export class ToDoList {
+
+    constructor() {
+        
+        this.todos = [];
+
+    }
+    
+    nuevoToDo(todo) {
+        this.todos.push(todo);    
+    }
+    
+    eliminarToDo (id){
+    
+    }
+    
+    marcarCompletado(id){
+        
+    }
+
+    eliminarCompletados (){
+        
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { saludar } from './js/componentes';
 import './styles.css';
+import {ToDo, ToDoList} from './classes';
 
-const nombre = 'Fernando';
+const toDoList = new ToDoList();
+const tarea = new ToDo('Aprender JavaScript');
+const tarea2 = new ToDo('Comprar un unicornio');
 
-saludar( nombre );
+toDoList.nuevoToDo(tarea);
+toDoList.nuevoToDo(tarea2);
+
+console.log (tarea);
+console.log (tarea2);
+console.log (toDoList);
